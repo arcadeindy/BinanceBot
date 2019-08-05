00f3810ea5b0070425d005c04b4fa72dd0794b6c
flaxmine9 <51380725+flaxmine9@users.noreply.github.com>
2019-08-05 14:09:09 +0300

    Update SettingsAPI.cs

--- Model/WorkCryptoBirge/API/SettingsAPI.cs
+++ Model/WorkCryptoBirge/API/SettingsAPI.cs
@@ -3,7 +3,7 @@
     public static class SettingsAPI
     {
         public static string baseUrl { get; set; } = "https://www.binance.com/api/";
-        public static string ApiKey { get; set; } = "p7EzwnnXpM3YblWENlyeLtpvgIPCspf0bvp5ShEUDjcNyPaFbMuxswrk4NX5lcJ5";
-        public static string SecretKey { get; set; } = "D3tI1bRVLPqWU1S7kKOp1cHjVQqb1fuDDkbzk9TQa4kfdJ5ALFfAVFv1pnFPnWZ1";
+        public static string ApiKey { get; set; } = "";
+        public static string SecretKey { get; set; } = "";
     }
 }
